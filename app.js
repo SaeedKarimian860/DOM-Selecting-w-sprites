3f8043c52f6abb451a005425552772a88f0b9801
SaeedKarimian860 <SGK12190@gmail.com>
2025-01-17 23:56:18 -0500

    DOM selecting w/ Sprites

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 //https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
 
 const container = document.querySelector('#container');
-const newImg = document.createElement('img');
-newImg.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png';
-container.appendChild(newImg)
+const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+
+
+for (let i = 1; i <= 151; i++) {
+    const pokemon = document.createElement('div');
+    const label = document.createElement('span');
+    label.innerText = `#${i}`;
+    const newImg = document.createElement('img');
+    newImg.src = `${baseURL}${i}.png`
+
+    pokemon.appendChild(newImg);
+    pokemon.appendChild(label);
+    container.appendChild(pokemon)
+}
+ 
+/* <div>
+    <img>
+    <span>2</span>
+    </img>
+</div>  */
